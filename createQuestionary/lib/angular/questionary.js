e0b60bb29f7c3a175e120027045830998291c3e6
ArthurSampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2016-08-26 00:03:18 -0300

    solved problem, now make magic baby

--- createQuestionary/lib/angular/questionary.js
+++ createQuestionary/lib/angular/questionary.js
@@ -4,20 +4,25 @@ angular.module("questionary").factory("formService", function () {
 	formService = {};
 
 	formService.getQuestions = function(){
-		var questions = ["Primeira Questão", "Segunda Questão"];
+		var questions = ["Lamento Dizer que deu certo", "Quenhé que manja do rolé?"];
 		return questions;
 	}
+
+	return formService;
 
 });
 
 
 angular.module("questionary").controller("formCtrl",
 	function($scope, formService){
-		$scope.questions = formService.getQuestions();
+		
+		var begin = function(){
+			$scope.questions = formService.getQuestions();
+			$scope.ab = "A SOCIENDAND SENRA DENRONTADA PENLA VINTORIA DON MAKENGUMI";
+		};
 	
 
-	$scope.a = ['ajuheaeh', 'auehuaeh'];
-
+	
 	begin();
 
 });
