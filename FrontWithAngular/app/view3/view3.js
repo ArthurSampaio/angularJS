d461cef2ffe3853991cdedf4ed51e854d349886a
ArthurSampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2016-09-30 18:32:38 -0300

    i fixed the bug in view3

--- FrontWithAngular/app/view3/view3.js
+++ FrontWithAngular/app/view3/view3.js
@@ -30,37 +30,45 @@ angular.module("myApp.view3", ['ngRoute'])
 
 	})
 
-	.controller('View3Ctrl',[function($scope, formService){
-			
-			var begin = function(){
-				$scope.questions = formService.getQuestions();
-				$scope.types = formService.getType();
-			};
+	.controller('View3Ctrl',['$scope',function($scope, formService){
+
+
+			var begin =  function(){
+
+				$scope.questions =  [{message:"Os pré-requisitos assumidos pela disciplina foram adequados?"},
+							 {message:"O programa da disciplina esteve de acordo com a ementa da mesma?"}
+							];
+				$scope.types = [{name: "Múltipla Escolha"},
+					 	{name: "Escolha Única"},
+					 	{name: "Apenas Texto"}
+						];
+			}
+
+
+
+			$scope.addQuestion = function(question){
+
+				$scope.questions.push(angular.copy(question)); //Adiciona no fim do array			
+				delete $scope.question;
+			}
+
 
 			$scope.isQuestionSelected = function (questions){
 					return questions.some(function (question){
 						return question.selected;
 				});
-			};
+			}
 
 
 			$scope.deleteQuestion = function(questions){
 					$scope.questions = questions.filter(function(question){
 						if (!question.selected) return question;
 				})	
-			};
-
-			
+			}
 
 
-			$scope.addQuestion = function(question){
-				$scope.questions.push(angular.copy(question)); //Adiciona no fim do array			
-				delete $scope.question
-			};
+			begin();
 
-			
-		
-		begin();
 
 	}])
 
